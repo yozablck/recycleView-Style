e93267f12a84e7ac7931585905598c2c485adec8
yozablck <yosiayh23@gmail.com>
2022-05-11 08:27:58 +0700

    recyle view dan style

---
+++ app/src/main/java/com/asthiseta/myrecyclerview/MainActivity.kt
@@ -0,0 +1,76 @@
+package com.asthiseta.myrecyclerview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.asthiseta.myrecyclerview.adapter.CardViewHeroAdapter
+import com.asthiseta.myrecyclerview.adapter.GridHeroAdapter
+import com.asthiseta.myrecyclerview.adapter.ListHeroAdapter
+import com.asthiseta.myrecyclerview.model.Hero
+import com.asthiseta.myrecyclerview.model.HeroesData
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var rvHeroes: RecyclerView
+    private var list: ArrayList<Hero> = arrayListOf()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(HeroesData.listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
+        supportActionBar?.title = getString(R.string.txt_list)
+    }
+
+    private fun showRecyclerGrid(){
+        rvHeroes.layoutManager = GridLayoutManager(this, 2)
+        val gridHeroAdapter = GridHeroAdapter(list)
+        rvHeroes.adapter = gridHeroAdapter
+        supportActionBar?.title = getString(R.string.txt_grid)
+    }
+
+    private fun showRecyclerCardView() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val cardViewHeroAdapter = CardViewHeroAdapter(list)
+        rvHeroes.adapter = cardViewHeroAdapter
+        supportActionBar?.title = getString(R.string.txt_with_cardview)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+                showRecyclerList()
+            }
+
+            R.id.action_grid -> {
+                showRecyclerGrid()
+            }
+
+            R.id.action_cardview -> {
+                showRecyclerCardView()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/asthiseta/myrecyclerview/model/Hero.kt
@@ -0,0 +1,7 @@
+package com.asthiseta.myrecyclerview.model
+
+data class Hero(
+    var name: String = "",
+    var detail: String = "",
+    var photo: Int = 0
+)
